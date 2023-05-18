a8fd2e5ba806c147135a578c6c24035373e77cb9
Hamdhani Vaniza <redstonesandhamdhani@gmail.com>
2023-05-18 12:11:46 +0700

    Update Password generator.py

--- Password generator.py
+++ Password generator.py
@@ -1,8 +1,10 @@
 import random
-x="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-panjang_sandi=int(input("masukan panjang password:"))
-password=""
-for i in range(panjang_sandi):
-    password = password + random.choice(x)
-
-print (password)
+y=input("Start Generate?:")
+while y == "yes" or y == "y" or y == "ya":
+    x="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    panjang_sandi=int(input("masukan panjang password:"))
+    password=""
+    for i in range(panjang_sandi):
+        password = password + random.choice(x)
+    print (password)
+    y=input("Continue?:")
